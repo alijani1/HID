195c2afcfbdd3cb66751c4f929999ff21b456541
Ali Jani <ajanimail@gmail.com>
2021-02-24 20:07:57 -0500

    Update SingleGamepad.cpp

--- src/SingleReport/SingleGamepad.cpp
+++ src/SingleReport/SingleGamepad.cpp
@@ -31,11 +31,11 @@ static const uint8_t _hidReportDescriptorGamepad[] PROGMEM = {
 	/* 32 Buttons */
 	0x05, 0x09,							/*   USAGE_PAGE (Button) */
 	0x19, 0x01,							/*   USAGE_MINIMUM (Button 1) */
-	0x29, 0x10,							/*   USAGE_MAXIMUM (Button 32) */
+	0x29, 0x10,							/*   USAGE_MAXIMUM (Button 16) */
 	0x15, 0x00,							/*   LOGICAL_MINIMUM (0) */
 	0x25, 0x01,							/*   LOGICAL_MAXIMUM (1) */
 	0x75, 0x01,							/*   REPORT_SIZE (1) */
-	0x95, 0x20,							/*   REPORT_COUNT (32) */
+	0x95, 0x10,							/*   REPORT_COUNT (16) */
 	0x81, 0x02,							/*   INPUT (Data,Var,Abs) */
 	/* 4 16bit Axis */
 	0x05, 0x01,							/*   USAGE_PAGE (Generic Desktop) */
