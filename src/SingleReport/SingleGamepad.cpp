40f120d6f9b095f6a4e237a01621cff9c94a67b7
Ali Jani <ajanimail@gmail.com>
2021-02-24 20:06:55 -0500

    Update SingleGamepad.cpp
    testing 16 button instead of 32

--- src/SingleReport/SingleGamepad.cpp
+++ src/SingleReport/SingleGamepad.cpp
@@ -31,7 +31,7 @@ static const uint8_t _hidReportDescriptorGamepad[] PROGMEM = {
 	/* 32 Buttons */
 	0x05, 0x09,							/*   USAGE_PAGE (Button) */
 	0x19, 0x01,							/*   USAGE_MINIMUM (Button 1) */
-	0x29, 0x20,							/*   USAGE_MAXIMUM (Button 32) */
+	0x29, 0x10,							/*   USAGE_MAXIMUM (Button 32) */
 	0x15, 0x00,							/*   LOGICAL_MINIMUM (0) */
 	0x25, 0x01,							/*   LOGICAL_MAXIMUM (1) */
 	0x75, 0x01,							/*   REPORT_SIZE (1) */
