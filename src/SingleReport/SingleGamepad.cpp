4cbb4833de3428757f02a5fc58b6a5abc79f5f32
Ali Jani <ajanimail@gmail.com>
2021-03-28 20:50:48 -0400

    descriptor updated

--- src/SingleReport/SingleGamepad.cpp
+++ src/SingleReport/SingleGamepad.cpp
@@ -31,11 +31,11 @@ static const uint8_t _hidReportDescriptorGamepad[] PROGMEM = {
 	/* 32 Buttons */
 	0x05, 0x09,							/*   USAGE_PAGE (Button) */
 	0x19, 0x01,							/*   USAGE_MINIMUM (Button 1) */
-	0x29, 0x10,							/*   USAGE_MAXIMUM (Button 32) */
+	0x29, 0x20,							/*   USAGE_MAXIMUM (Button 32) */
 	0x15, 0x00,							/*   LOGICAL_MINIMUM (0) */
 	0x25, 0x01,							/*   LOGICAL_MAXIMUM (1) */
 	0x75, 0x01,							/*   REPORT_SIZE (1) */
-	0x95, 0x10,							/*   REPORT_COUNT (32) */
+	0x95, 0x20,							/*   REPORT_COUNT (32) */
 	0x81, 0x02,							/*   INPUT (Data,Var,Abs) */
 	/* 2 16bit Axis */
 	0x05, 0x01,							/*   USAGE_PAGE (Generic Desktop) */
